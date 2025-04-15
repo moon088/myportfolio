7983639182c0f71b212581839dd67d6f96f6c05b
moon088 <naoyabsktbl@gamil.com>
2025-04-15 18:00:05 +0900

    s

--- JS/animation.js
+++ JS/animation.js
@@ -1,21 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const faders = document.querySelectorAll('.fade-in');
-    const options = {
-      threshold: 0.2
-    };
-  
-    const appearOnScroll = new IntersectionObserver(function (entries) {
-      entries.forEach(entry => {
+  const faders = document.querySelectorAll('.fade-in');
+  let userScrolled = false;
+
+  faders.forEach(fader => {
+    fader.classList.remove('visible');
+  });
+
+  window.addEventListener('scroll', () => {
+    userScrolled = true;
+  });
+
+  const options = {
+    threshold: 0.3  // 20%見えたら反応
+  };
+
+  const appearOnScroll = new IntersectionObserver(function (entries) {
+    entries.forEach(entry => {
+      if (entry.target === faders[0] || userScrolled) {
         if (entry.isIntersecting) {
           entry.target.classList.add('visible');
         } else {
-          entry.target.classList.remove('visible'); 
+          entry.target.classList.remove('visible');
         }
-      });
-    }, options);
-  
-    faders.forEach(fader => {
-      appearOnScroll.observe(fader);
+      }
     });
+  }, options);
+
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
   });
-  
+});
