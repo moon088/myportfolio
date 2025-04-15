64a562e980f4cf494192ff6406ce1be8bbe857fe
moon088 <naoyabsktbl@gamil.com>
2025-04-15 18:17:50 +0900

    s

--- JS/animation.js
+++ JS/animation.js
@@ -11,11 +11,8 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   const options = {
-<<<<<<< HEAD
-    threshold: 0.3  // 20%見えたら反応
-=======
     threshold: 0.2  // 20%見えたら反応
->>>>>>> 3cf00a3113f4f755f4db6e9a6ce4259e144d007a
+
   };
 
   const appearOnScroll = new IntersectionObserver(function (entries) {
